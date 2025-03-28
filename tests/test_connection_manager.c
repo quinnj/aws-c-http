d2d34aecb5fb3ccf91d3245af36196adc5486163
Jacob Quinn <quinn.jacobd@gmail.com>
2025-03-28 16:15:35 -0600

    Fix bad rebase from websocket server code

--- tests/test_connection_manager.c
+++ tests/test_connection_manager.c
@@ -161,7 +161,12 @@ static int s_cm_tester_init(struct cm_tester_options *options) {
         clock_fn = options->mock_table->aws_high_res_clock_get_ticks;
     }
 
-    tester->event_loop_group = aws_event_loop_group_new(tester->allocator, clock_fn, 1, NULL, NULL, NULL);
+    struct aws_event_loop_group_options elg_options = {
+        .loop_count = 1,
+        .clock_override = clock_fn,
+    };
+
+    tester->event_loop_group = aws_event_loop_group_new(tester->allocator, &elg_options);
 
     struct aws_host_resolver_default_options resolver_options = {
         .el_group = tester->event_loop_group,
